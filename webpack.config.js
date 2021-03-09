79200545a7dd942343e77be0775a3a71dd5b86dc
Syellie <dsam69140@gmail.com>
2021-03-09 10:17:30 +0100

    ajouts supp

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,8 @@ module.exports = {
             port: 3000,
             files: [
                 // 'www/index.html',
-                'www/trailselect.html'
+                'www/trailselect.html',
+                'www/home.html'
                 // add other files to watch for hot reload
             ],
             server: {
