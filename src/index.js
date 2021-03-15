4ea00068a9f3a1bdb281be500a200b9f1b8b8f13
HugoBoxFr <preahugo@gmail.com>
2021-03-15 16:02:21 +0100

    map display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'materialize-css';
 import { displayNav } from './shared/navComponent/nav.component';
 import { navRouting } from './shared/navComponent/nav-routing';
+import { displayMapComponent } from './mapComponent/map.component';
 
 // navbar
 displayNav();
 navRouting();
+displayMapComponent("main");
