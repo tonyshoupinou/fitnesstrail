de25ebd9f7564421bdfae3733e1179ca6b58bafa
HugoBoxFr <preahugo@gmail.com>
2021-03-15 15:55:01 +0100

    position map

--- src/mapComponent/initMap.js
+++ src/mapComponent/initMap.js
@@ -1,26 +1,46 @@
 import { myPosition } from "./watchPosition";
 
+let marker = null;
+let geoLoc;
+let watchID;
+
 export const initMap = () => {
     const showPosition = (position) => {
       let coords = { lat: position.coords.latitude, lng: position.coords.longitude};
+
       const map = new google.maps.Map(document.querySelector(".display-map"), {
         zoom: 15,
         center: coords,
       });
   
-      const marker = new google.maps.Marker({
+      marker = new google.maps.Marker({
         position: coords,
         map: map,
+
+        // recentrer carte et evoluer position marker
       });
+
+      let options = {enableHighAccuracy: true};
+      geoLoc = navigator.geolocation;
+      watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
+    }
+
+    const showLocation = (position) => {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      console.log("Latitude : " + latitude + " Longitude: " + longitude);
+    }
+    
+    const errorHandler = (err) => {
+      if(err.code == 1) {
+          alert("Error: Access is denied!");
+      } else if( err.code == 2) {
+          alert("Error: Position is unavailable!");
+      }
     }
     
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-        const id = navigator.geolocation.watchPosition(position => {
-            console.log(position)
-
-        })
-        console.log(id)
     } else {
       map.innerHTML = "Geolocation is not supported by this browser.";
     }
--- src/mapComponent/watchPosition.js
+++ src/mapComponent/watchPosition.js
@@ -1,6 +1,31 @@
 export const myPosition = () => {
-    const id = navigator.geolocation.watchPosition(position => {
-        console.log(position)
-        
-    })
+    var id, target, options;
+
+    function success(pos) {
+        var crd = pos.coords;
+
+        if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+            console.log('Bravo, vous avez atteint la cible');
+            navigator.geolocation.clearWatch(id);
+        }
+    }
+
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    }
+
+    target = {
+        latitude : 0,
+        longitude: 0
+    };
+
+    options = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    id = navigator.geolocation.watchPosition(success, error, options);
+
+    console.log(id);
 }
